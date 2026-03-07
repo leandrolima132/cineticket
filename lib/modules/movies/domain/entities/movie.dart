64e58b9bee26a72db5167cf1e98699cf560b7410
Leandro Ferreira <leandro.ferreira@vocedm.com.br>
2026-03-07 17:56:29 -0300

    feat(movie): add Movie domain entity with core properties

---
+++ lib/modules/movies/domain/entities/movie.dart
@@ -0,0 +1,35 @@
+import 'package:equatable/equatable.dart';
+
+class Movie extends Equatable {
+  final String id;
+  final String title;
+  final String description;
+  final String posterUrl;
+  final String duration;
+  final String genres;
+  final String rating;
+  final String releaseDate;
+
+  const Movie({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.posterUrl,
+    required this.duration,
+    required this.genres,
+    required this.rating,
+    required this.releaseDate,
+  });
+
+  @override
+  List<Object?> get props => [
+        id,
+        title,
+        description,
+        posterUrl,
+        duration,
+        genres,
+        rating,
+        releaseDate
+      ];
+}
