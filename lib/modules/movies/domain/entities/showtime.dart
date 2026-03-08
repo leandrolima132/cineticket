ff9605f917ecef992db2c5eccf0723fd87eaabc3
Leandro Ferreira <leandro.ferreira@vocedm.com.br>
2026-03-07 22:02:04 -0300

    feat(movie): add Showtime domain entity with core properties

---
+++ lib/modules/movies/domain/entities/showtime.dart
@@ -0,0 +1,23 @@
+import 'package:equatable/equatable.dart';
+
+class Showtime extends Equatable {
+  final String id;
+  final String movieId;
+  final String theater;
+  final String room;
+  final DateTime dateTime;
+  final int availableSeats;
+
+  const Showtime({
+    required this.id,
+    required this.movieId,
+    required this.theater,
+    required this.room,
+    required this.dateTime,
+    required this.availableSeats,
+  });
+
+  @override
+  List<Object?> get props =>
+      [id, movieId, theater, room, dateTime, availableSeats];
+}
