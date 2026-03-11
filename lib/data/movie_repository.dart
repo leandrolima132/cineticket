a019dcbbae38291a7b56d2a511cde7c3eb99ce75
Leandro Ferreira <leandro.ferreira@vocedm.com.br>
2026-03-11 17:33:51 -0300

    feat(data) add MovieRepository

---
+++ lib/data/movie_repository.dart
@@ -0,0 +1,22 @@
+import 'package:cineticket/data/models/movie.dart';
+import 'package:cineticket/data/models/showtime.dart';
+import 'package:cineticket/data/mocks/movie_mock.dart';
+import 'package:cineticket/data/mocks/showtime_mock.dart';
+
+class MovieRepository {
+  Future<List<Movie>> getMovies() async {
+    await Future.delayed(const Duration(milliseconds: 500));
+    return MovieMock.moviesList;
+  }
+
+  Future<Movie> getMovieDetails(String movieId) async {
+    await Future.delayed(const Duration(milliseconds: 300));
+    final movie = MovieMock.moviesList.firstWhere((m) => m.id == movieId);
+    return movie;
+  }
+
+  Future<List<Showtime>> getShowtimes(String movieId) async {
+    await Future.delayed(const Duration(milliseconds: 300));
+    return ShowtimeMock.getByMovieId(movieId);
+  }
+}
